73f5ed207166b59fc2745909ec4c0c9f5d46d340
kpetridis <petridkon@gmail.com>
2022-05-12 18:11:09 +0300

    Final corrections

--- inc/Helpers/reader.py
+++ inc/Helpers/reader.py
@@ -14,10 +14,16 @@ def load_data_npy(filename):
 
     Returns
     -------
-    verts2d_final : the coordinates of all the vertices of the triangle
+    verts3d : the 3D coordinates of all the vertices of the triangle
     vcolors : the RGB colors of the three vertices, of this triangle
     faces : the indices from the three vertices of every triangle
-    depth : the depth of every vertex
+    u : the axis of rotation
+    ck : the pointing coordinates of the camera
+    cu : the upp-vector of the camera
+    cv : the coordinates of the camera
+    t1 : displacement transformation
+    t2 : displacement transformation
+    phi : the rotation angle in radians
     """
     data = np.load(filename, allow_pickle=True).tolist()
     data = dict(data)
@@ -34,24 +40,29 @@ def load_data_npy(filename):
 def load_data_mat(filename):
     """Loads the necessary data from a matlab file.
 
-        Parameters
-        ----------
-        filename : the name of the file, including the .npy extension.
+    Parameters
+    ----------
+    filename : the name of the file, including the .npy extension.
 
-        Returns
-        -------
-        verts2d_final : the coordinates of all the vertices of the triangle
-        vcolors : the RGB colors of the three vertices, of this triangle
-        faces : the indices from the three vertices of every triangle
-        depth : the depth of every vertex
+    Returns
+    -------
+    verts3d : the 3D coordinates of all the vertices of the triangle
+    vcolors : the RGB colors of the three vertices, of this triangle
+    faces : the indices from the three vertices of every triangle
+    u : the axis of rotation
+    ck : the pointing coordinates of the camera
+    cu : the upp-vector of the camera
+    cv : the coordinates of the camera
+    t1 : displacement transformation
+    t2 : displacement transformation
+    phi : the rotation angle in radians
     """
     data = io.loadmat(filename)
 
-    C, F, V, O = np.array(data['C']), np.array(data['F'] - 1), np.array(data['V']), np.array(data['O'])
-    H, W, M, N = data['H'][0, 0], data['W'][0, 0], data['M'][0, 0], data['N'][0, 0]
-    w, g = data['w'][0, 0], data['g'].T[0]
+    vcolors, faces, verts3d = np.array(data['C']), np.array(data['F'] - 1), np.array(data['V'])
+    u = data['g'].T[0]
     ck, cu, cv = data['ck'].T[0], data['cu'].T[0], data['cv'].T[0]
     t1, t2 = data['t1'].T[0], data['t2'].T[0]
     phi = data['theta'][0]
 
-    return C, F, V, g, ck, cu, cv, t1, t2, phi
+    return vcolors, faces, verts3d, u, ck, cu, cv, t1, t2, phi
